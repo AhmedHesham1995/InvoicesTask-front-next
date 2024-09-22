08bca6e0d351e4f31929c34958703600eda5f8d1
ahmedhesham1995 <ahmed.hesham.ahmed10@gmail.com>
2024-09-22 13:21:33 +0300

    push

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,49 @@
 import Head from "next/head";
-import Image from "next/image";
-import localFont from "next/font/local";
+
 import styles from "@/styles/Home.module.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 export default function Home() {
+  const [invoices, setInvoices] = useState([]);
+  const [newInvoice, setNewInvoice] = useState({
+    lineNo: "",
+    productName: "",
+    unitNo: "",
+    price: "",
+    quantity: "",
+    expiryDate: "",
+  });
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await axios.get("http://localhost:5000/Invoices");
+      console.log(response);
+      setInvoices(response.data);
+      console.log(invoices);
+    };
+
+    fetchProducts();
+  }, []);
+
+  const handleDelete = async (id) => {
+    await axios.delete(`http://localhost:5000/Invoices/${id}`);
+    const fetchProducts = async () => {
+      const response = await axios.get("http://localhost:5000/Invoices");
+      console.log(response);
+      setInvoices(response.data);
+    };
+    fetchProducts();
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:5000/Invoices", newInvoice);
+    const fetchProducts = async () => {
+      const response = await axios.get("http://localhost:5000/Invoices");
+      console.log(response);
+      setInvoices(response.data);
+    };
+    fetchProducts();
+  };
   return (
     <>
       <Head>
@@ -23,95 +52,86 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div
-        className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
-      >
-        <main className={styles.main}>
-          <Image
-            className={styles.logo}
-            src="https://nextjs.org/icons/next.svg"
-            alt="Next.js logo"
-            width={180}
-            height={38}
-            priority
-          />
-          <ol>
-            <li>
-              Get started by editing <code>pages/index.js</code>.
-            </li>
-            <li>Save and see your changes instantly.</li>
-          </ol>
+      <h1>Hello</h1>
 
-          <div className={styles.ctas}>
-            <a
-              className={styles.primary}
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                className={styles.logo}
-                src="https://nextjs.org/icons/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-              />
-              Deploy now
-            </a>
-            <a
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.secondary}
-            >
-              Read our docs
-            </a>
-          </div>
-        </main>
-        <footer className={styles.footer}>
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="https://nextjs.org/icons/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
-            Learn
-          </a>
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="https://nextjs.org/icons/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
-            Examples
-          </a>
-          <a
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="https://nextjs.org/icons/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to nextjs.org →
-          </a>
-        </footer>
+      <div>
+        <h1>List of products</h1>
+        <ul>
+          {invoices.map((e) => (
+            <>
+              <li>
+                {e.productName} - {e.price} - {e.quantity} - {e.total} -{" "}
+                {e.lineNo}
+              </li>
+              <button onClick={() => handleDelete(e._id)}>Delete</button>
+            </>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <h1>Add products</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="product name"
+            value={newInvoice.productName}
+            onChange={(e) =>
+              setNewInvoice({ ...newInvoice, productName: e.target.value })
+            }
+          />
+          <input
+            type="number"
+            placeholder="Line Number"
+            value={newInvoice.lineNo}
+            onChange={(e) =>
+              setNewInvoice({ ...newInvoice, lineNo: e.target.value })
+            }
+            required
+          />
+
+          <input
+            type="text"
+            placeholder="Unit ObjectId"
+            value={newInvoice.unitNo}
+            onChange={(e) =>
+              setNewInvoice({ ...newInvoice, unitNo: e.target.value })
+            }
+            required
+          />
+
+          <input
+            type="number"
+            placeholder="Price"
+            value={newInvoice.price}
+            onChange={(e) =>
+              setNewInvoice({ ...newInvoice, price: e.target.value })
+            }
+            required
+          />
+
+          <input
+            type="number"
+            placeholder="Quantity"
+            value={newInvoice.quantity}
+            onChange={(e) =>
+              setNewInvoice({ ...newInvoice, quantity: e.target.value })
+            }
+            required
+          />
+
+          <input
+            type="date"
+            placeholder="Expiry Date"
+            value={newInvoice.expiryDate}
+            onChange={(e) =>
+              setNewInvoice({ ...newInvoice, expiryDate: e.target.value })
+            }
+            required
+          />
+
+          <button type="submit">add</button>
+        </form>
       </div>
     </>
   );
